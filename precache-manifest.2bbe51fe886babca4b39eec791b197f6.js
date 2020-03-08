cef2dd9ad914b1b47e75955b9f331e1cce339e9a
DavidKe91 <davidgkennedy@hotmail.com>
2020-03-08 21:24:30 +0000

    Updates

---
+++ precache-manifest.2bbe51fe886babca4b39eec791b197f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878f74f94ccbbaae33bcb2a97f93ee04",
+    "url": "/react-burger-builder/index.html"
+  },
+  {
+    "revision": "df88edeb48196ee77b40",
+    "url": "/react-burger-builder/static/css/main.fdef324d.chunk.css"
+  },
+  {
+    "revision": "a2c7117afb7e01043f8d",
+    "url": "/react-burger-builder/static/js/2.fdb15759.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/react-burger-builder/static/js/2.fdb15759.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df88edeb48196ee77b40",
+    "url": "/react-burger-builder/static/js/main.e2b4dbcc.chunk.js"
+  },
+  {
+    "revision": "674e4b8ab2c3ed8466cd",
+    "url": "/react-burger-builder/static/js/runtime-main.75ccacc0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
